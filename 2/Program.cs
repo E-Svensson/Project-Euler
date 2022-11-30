2450e11cb69f665719ad3f0a289905e6d551df6c
japo04001 <japo04001@utb.vaxjo.se>
2022-11-30 09:08:29 +0100

    2 klar

--- 2/Program.cs
+++ 2/Program.cs
@@ -6,7 +6,31 @@ namespace ____
     {
         static void Main(string[] args)
         {
+            int[] num = { 1, 2 };
+            int Summa = 2;
 
+            // num[0] = 1
+            // num[1] = 2
+
+            int toggleValue = (0 ^ 1);
+
+            int a = 0;
+            int b = 1;
+
+            while (num[0] < 4000000 || num[1] < 4000000)
+            {
+                num[a] = num[a] + num[b];
+
+                if (num[a] % 2 == 0)
+                {
+                    Summa += num[a];
+                }
+
+                a ^= toggleValue;
+                b ^= toggleValue;
+            }
+
+            Console.WriteLine(Summa);
         }
     }
 }
