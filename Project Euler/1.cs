6271bf93d643e7393a084a7faf4d4d43383e947d
japo04001 <japo04001@utb.vaxjo.se>
2022-11-30 08:35:08 +0100

    Add project files.

---
+++ Project Euler/1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Summa = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    Summa += i;
+                }
+            }
+
+            Console.WriteLine(Summa);
+        }
+    }
+}
