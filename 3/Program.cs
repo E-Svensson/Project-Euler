2a4b2147967d51eb75aa13b349f1dd987fc7101d
japo04001 <japo04001@utb.vaxjo.se>
2022-11-30 10:45:16 +0100

    3 påbörjad

---
+++ 3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long num = 13195; // 600851475143
+
+
+        }
+    }
+}
