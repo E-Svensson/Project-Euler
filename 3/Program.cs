7525a0d6448fd2c6e0d0eb67b447b463a7dc9c12
japo04001 <111962656+Dungeater1337@users.noreply.github.com>
2022-12-16 10:54:34 +0100

    3 klar

--- 3/Program.cs
+++ 3/Program.cs
@@ -6,9 +6,20 @@ namespace ____
     {
         static void Main(string[] args)
         {
-            long num = 13195; // 600851475143
+            long num = 600851475143; // 600851475143
 
+            List<int> primes = new List<int>();
 
+            double sqrt = Math.Sqrt(num);
+
+            for (int i = 2; i < sqrt; i++)
+            {
+                if (num % i == 0)
+                {
+                    num = num / i;
+                    Console.WriteLine(i);
+                }
+            }
         }
     }
 }
